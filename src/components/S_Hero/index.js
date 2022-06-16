c2a52073012d6ed7b94135e1344f916cdd92ffd2
tvoygluk <tvoygluk@gmail.com>
2022-06-16 13:44:34 +0300

    Разработка рентген-эффекта для блока hero - черновик

--- src/components/S_Hero/index.js
+++ src/components/S_Hero/index.js
@@ -3,9 +3,48 @@ import classNames from "classnames";
 import {logo} from '../../img/svgInlineImg.js'
 import {links} from "../../data/stoneLinks";
 import {C_MainButton} from "../C_MainButton";
+import {useRef} from "react";
 
 export const S_Hero = ({className}) => {
+    const interactiveBlock = useRef(null);
+    const topPic = useRef(null);
     const cls = classNames(classes.root, {[className]: className });
+    const updateCursor = (e, topPic, interactiveBlock) => {
+        const rect = interactiveBlock.current.getBoundingClientRect()
+
+        // console.log('Координата начала блока по X относительно документа:', rect.x + pageXOffset)
+        // console.log('координата начала блока по Y относительно документа:', rect.y + pageYOffset)
+        // console.log('Ширина блока:', topPic.current.clientWidth)
+        // console.log('Высота блока:', topPic.current.clientHeight)
+        // // console.log('Координата конца блока по X относительно документа:', rect.x + pageXOffset + topPic.current.clientWidth)
+        // // console.log('Координата конца блока по Y относительно документа:', rect.y + pageYOffset + topPic.current.clientHeight)
+        // console.log('Позиция курсора по X', e.pageX)
+        // console.log('Позиция курсора по Y', e.pageY)
+
+        let xBlockPercent = 0
+        let yBlockPercent = 0
+        xBlockPercent = Math.abs((e.pageX - (rect.x + pageXOffset))/(interactiveBlock.current.clientWidth / 100))
+        yBlockPercent = Math.abs((e.pageY - (rect.y + pageYOffset))/(interactiveBlock.current.clientHeight / 100))
+        // console.log('Позиция курсора по X в % относительно блока', xBlockPercent)
+        // console.log('Позиция курсора по Y в % относительно блока', yBlockPercent)
+
+
+        // topPic.current.style.clipPath = `circle(230px at ${xBlockPercent}% ${yBlockPercent}%);`
+
+        // topPic.current.style['clip-path'] = `circle(230px at ${xBlockPercent}% ${yBlockPercent}%)`
+        // console.log(`circle(230px at ${xBlockPercent}% ${yBlockPercent}%)`, topPic.current.style['clip-path'])
+        // todo: сделать динамический размер
+        topPic.current.style['clip-path'] = `circle(${xBlockPercent * 2}px at ${xBlockPercent}% ${yBlockPercent}%)`
+
+        // if (xBlockPercent <=10 || yBlockPercent <= 10) {
+        //     topPic.current.style['clip-path'] = `circle(30px at ${xBlockPercent}% ${yBlockPercent}%)`
+        // }
+        // else {
+        //     topPic.current.style['clip-path'] = `circle(230px at ${xBlockPercent}% ${yBlockPercent}%)`
+        // }
+
+
+    }
     return (
         <div className={cls} >
             <div className={classes.textBlock}>
@@ -20,9 +59,60 @@ export const S_Hero = ({className}) => {
                 </ul>
                 <C_MainButton text={"Получить предложение"} onClick={() => console.log('click')} className={classes.mainButton} />
             </div>
-            <div className={classes.interactiveBlock} >
+            <div
+                className={classes.interactiveBlock}
+                ref={interactiveBlock}
+                onMouseMove={(e) => updateCursor(e, topPic, interactiveBlock)}
+            >
                 <div className={classes.btmPic}/>
-                <div className={classes.topPic}/>
+                <div
+                    className={classes.topPic}
+                    ref={topPic}
+
+
+
+                    // onMouseDown={(e) => {
+                    //     const rect = topPic.current.getBoundingClientRect()
+                    //
+                    //     // console.log('Координата начала блока по X относительно документа:', rect.x + pageXOffset)
+                    //     // console.log('координата начала блока по Y относительно документа:', rect.y + pageYOffset)
+                    //     // console.log('Ширина блока:', topPic.current.clientWidth)
+                    //     // console.log('Высота блока:', topPic.current.clientHeight)
+                    //     // // console.log('Координата конца блока по X относительно документа:', rect.x + pageXOffset + topPic.current.clientWidth)
+                    //     // // console.log('Координата конца блока по Y относительно документа:', rect.y + pageYOffset + topPic.current.clientHeight)
+                    //     // console.log('Позиция курсора по X', e.pageX)
+                    //     // console.log('Позиция курсора по Y', e.pageY)
+                    //
+                    //     let xBlockPercent = Math.abs(Math.round((e.pageX - (rect.x + pageXOffset))/(topPic.current.clientWidth / 100)))
+                    //     let yBlockPercent = Math.abs(Math.round((e.pageY - (rect.y + pageYOffset))/(topPic.current.clientHeight / 100)))
+                    //     // console.log('Позиция курсора по X в % относительно блока', xBlockPercent)
+                    //     // console.log('Позиция курсора по Y в % относительно блока', yBlockPercent)
+                    //
+                    //
+                    //     topPic.current.style.clipPath = `circle(230px at ${xBlockPercent}% ${yBlockPercent}%);`
+                    // }}
+                    // onMouseUp={(e) => {
+                    //     const rect = topPic.current.getBoundingClientRect()
+                    //
+                    //     // console.log('Координата начала блока по X относительно документа:', rect.x + pageXOffset)
+                    //     // console.log('координата начала блока по Y относительно документа:', rect.y + pageYOffset)
+                    //     // console.log('Ширина блока:', topPic.current.clientWidth)
+                    //     // console.log('Высота блока:', topPic.current.clientHeight)
+                    //     // // console.log('Координата конца блока по X относительно документа:', rect.x + pageXOffset + topPic.current.clientWidth)
+                    //     // // console.log('Координата конца блока по Y относительно документа:', rect.y + pageYOffset + topPic.current.clientHeight)
+                    //     // console.log('Позиция курсора по X', e.pageX)
+                    //     // console.log('Позиция курсора по Y', e.pageY)
+                    //
+                    //     let xBlockPercent = Math.abs(Math.round((e.pageX - (rect.x + pageXOffset))/(topPic.current.clientWidth / 100)))
+                    //     let yBlockPercent = Math.abs(Math.round((e.pageY - (rect.y + pageYOffset))/(topPic.current.clientHeight / 100)))
+                    //     // console.log('Позиция курсора по X в % относительно блока', xBlockPercent)
+                    //     // console.log('Позиция курсора по Y в % относительно блока', yBlockPercent)
+                    //
+                    //
+                    //     topPic.current.style.clipPath = `circle(230px at ${xBlockPercent}% ${yBlockPercent}%);`
+                    // }}
+
+                />
             </div>
 
         </div>
