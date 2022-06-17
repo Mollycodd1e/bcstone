af9eb2d764914d9d22d6fdc279ed78fcfda9274c
tvoygluk <tvoygluk@gmail.com>
2022-06-17 11:36:16 +0300

    Разработка рентген-эффекта для блока hero - добавление задержки

--- src/components/S_Hero/index.js
+++ src/components/S_Hero/index.js
@@ -9,51 +9,38 @@ export const S_Hero = ({className}) => {
     const interactiveBlock = useRef(null);
     const topPic = useRef(null);
     const cls = classNames(classes.root, {[className]: className });
-    const updateCursor = (e, topPic, interactiveBlock) => {
+    const updateCursor = (e, topPic, interactiveBlock, customCircleSize = undefined) => {
         const rect = interactiveBlock.current.getBoundingClientRect()
-
-        // console.log('Координата начала блока по X относительно документа:', rect.x + pageXOffset)
-        // console.log('координата начала блока по Y относительно документа:', rect.y + pageYOffset)
-        // console.log('Ширина блока:', topPic.current.clientWidth)
-        // console.log('Высота блока:', topPic.current.clientHeight)
-        // // console.log('Координата конца блока по X относительно документа:', rect.x + pageXOffset + topPic.current.clientWidth)
-        // // console.log('Координата конца блока по Y относительно документа:', rect.y + pageYOffset + topPic.current.clientHeight)
-        // console.log('Позиция курсора по X', e.pageX)
-        // console.log('Позиция курсора по Y', e.pageY)
-
         let xBlockPercent = 0
         let yBlockPercent = 0
         xBlockPercent = Math.abs((e.pageX - (rect.x + pageXOffset))/(interactiveBlock.current.clientWidth / 100))
         yBlockPercent = Math.abs((e.pageY - (rect.y + pageYOffset))/(interactiveBlock.current.clientHeight / 100))
 
-        // console.log('Позиция курсора по X в % относительно блока', xBlockPercent)
-        // console.log('Позиция курсора по Y в % относительно блока', yBlockPercent)
+        if (customCircleSize === undefined) {
+            let size = 0;
+            const maxCircle = 4.1;
+            const limitCircle = maxCircle * 50;
+            const borderX = 40; // 0 - 50
+            const borderY = 20; // 0 - 50
+            const specialYCorrection = ((100 / 2) / borderY)
+            const specialXCorrection = ((100 / 2) / borderX)
+            const grownSpeed = 1.7;
+            const totalGrownSpeed = maxCircle * grownSpeed;
 
-        // topPic.current.style.clipPath = `circle(230px at ${xBlockPercent}% ${yBlockPercent}%);`
+            if (xBlockPercent <= borderX && yBlockPercent <= borderY) {
+                size = Math.min((totalGrownSpeed * Math.min((xBlockPercent * specialXCorrection), (yBlockPercent * specialYCorrection))), limitCircle)
+            } else if (xBlockPercent <= borderX && yBlockPercent > borderY) {
+                size = Math.min((totalGrownSpeed * Math.min((xBlockPercent * specialXCorrection), (100 - yBlockPercent))), limitCircle)
+            } else if (xBlockPercent > borderX && yBlockPercent > borderY) {
+                size = Math.min((totalGrownSpeed * Math.min((100 - xBlockPercent), (100 - yBlockPercent))), limitCircle)
+            } else {
+                size = Math.min((totalGrownSpeed * Math.min((100 - xBlockPercent), (yBlockPercent * specialYCorrection))), limitCircle)
+            }
 
-        // topPic.current.style['clip-path'] = `circle(230px at ${xBlockPercent}% ${yBlockPercent}%)`
-        // console.log(`circle(230px at ${xBlockPercent}% ${yBlockPercent}%)`, topPic.current.style['clip-path'])
-
-        let size = 0;
-        const maxCircle = 4.1;
-        const limitCircle = maxCircle * 50;
-        const borderX = 40; // 0 - 50
-        const borderY = 20; // 0 - 50
-        const specialYCorrection = ((100/2) / borderY)
-        const specialXCorrection = ((100/2) / borderX)
-        const grownSpeed = 1.7;
-        const totalGrownSpeed = maxCircle * grownSpeed;
-
-        if (xBlockPercent <= borderX && yBlockPercent <= borderY) {
-            size =  Math.min((totalGrownSpeed * Math.min((xBlockPercent * specialXCorrection), (yBlockPercent * specialYCorrection))), limitCircle)
-        } else if (xBlockPercent <= borderX && yBlockPercent > borderY) {
-            size = Math.min((totalGrownSpeed * Math.min((xBlockPercent * specialXCorrection), (100 - yBlockPercent))), limitCircle)
-        } else if  (xBlockPercent > borderX && yBlockPercent > borderY) {
-            size = Math.min((totalGrownSpeed * Math.min((100 - xBlockPercent), (100 - yBlockPercent))), limitCircle)
+            topPic.current.style['clip-path'] = `circle(${size}px at ${xBlockPercent}% ${yBlockPercent}%)`
         } else {
-            size = Math.min((totalGrownSpeed * Math.min((100 - xBlockPercent), (yBlockPercent * specialYCorrection))), limitCircle)
+            topPic.current.style['clip-path'] = `circle(${customCircleSize}px at ${xBlockPercent}% ${yBlockPercent}%)`
         }
-        topPic.current.style['clip-path'] = `circle(${size}px at ${xBlockPercent}% ${yBlockPercent}%)`
     }
     return (
         <div className={cls} >
@@ -120,10 +107,13 @@ export const S_Hero = ({className}) => {
                     // }}
                 />
                 <div className={classes.hover}
-                     onMouseMove={(e) => updateCursor(e, topPic, interactiveBlock)}
+                     onMouseMove={(e) => {
+                         updateCursor(e, topPic, interactiveBlock)
+                     }}
                      onMouseOut={(e) => {
-                         topPic.current.style['clip-path'] = `circle(0 at 0 0)`
-                     }}/>
+                         updateCursor(e, topPic, interactiveBlock, 0)
+                     }}
+                />
             </div>
 
         </div>
