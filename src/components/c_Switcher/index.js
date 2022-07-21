d51238c4c3f9df92c21382d0f2fad270e23195f7
tvoygluk <tvoygluk@gmail.com>
2022-07-21 15:10:50 +0300

    Вёрстка карточек - 10

--- src/components/c_Switcher/index.js
+++ src/components/c_Switcher/index.js
@@ -3,12 +3,14 @@ import classNames from "classnames";
 import {list, map} from "../../img/svgInlineImg";
 
 export const C_Switcher = ({className, isListView, setIsListView}) => {
-    const cls = classNames(classes.root, {[classes.mapView]: !isListView, [className]: className });
+    const cls = classNames(classes.root, {[classes.listViewActive]: isListView, [classes.mapViewActive]: !isListView, [className]: className });
     return (
         <div className={cls} onClick={() => setIsListView(prev => !prev)}>
-            <div className={classes.first} dangerouslySetInnerHTML={{ __html: list}} />
-            <div className={classes.second} dangerouslySetInnerHTML={{ __html: map}} />
-            <div className={classes.activeBg} />
+            <span className={classes.list}>списком</span>
+            <span className={classes.map}>на карте</span>
+            {/*<div className={classes.first} dangerouslySetInnerHTML={{ __html: list}} />*/}
+            {/*<div className={classes.second} dangerouslySetInnerHTML={{ __html: map}} />*/}
+            {/*<div className={classes.activeBg} />*/}
         </div>
     )
 }
