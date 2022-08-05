db7276fda5b986a09129d511d0dee242f3cffd41
Mollycodd1e <selik7@mail.ru>
2022-08-05 10:48:35 +0300

    тестовое изменение

--- src/sections/s_Menu/index.js
+++ src/sections/s_Menu/index.js
@@ -8,6 +8,7 @@ export const S_Menu = ({className, menuOnTop, data}) => {
     const cls = classNames(classes.root, {[classes.menuOnTop]:menuOnTop, [className]: className});
     return (
         <div className={cls}>
+            <div>Привет</div>
             <C_Logo className={classes.C_Logo_Menu} />
             <C_Navigation className={classes.C_Navigation} data={data} />
             <C_ContactsMenu className={classes.C_ContactsMenu} data={data} />
