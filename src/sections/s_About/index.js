ac6e4d3830c62ef618c34a4c6e16a3aaca7730a4
tvoygluk <tvoygluk@gmail.com>
2022-08-09 15:19:11 +0300

    Стилизация заголовка блока О нас - девелопер - 2

--- src/sections/s_About/index.js
+++ src/sections/s_About/index.js
@@ -6,11 +6,11 @@ export const S_About = ({className, menuOnTop, data}) => {
     const cls = classNames(classes.root, {[classes.menuOnTop]:menuOnTop, [className]: className});
     return (
         <div className={cls}>
-            <div className={classes.ProjectTitle}>
+            <div className={classes.frame}>
                 <div className={classes.bg_text}>
                     О нас
                 </div>
-                <div className={classes.wrap_title}>
+                <div className={classes.title}>
                     Девелопер
                 </div>
             </div>
