49becf8efd8cf729790a08420c908cbc6490cd55
tvoygluk <tvoygluk@gmail.com>
2022-08-12 17:15:54 +0300

    Разработка блока Девелопер - 7

--- src/components/c_SliderVideoAbout/index.js
+++ src/components/c_SliderVideoAbout/index.js
@@ -2,17 +2,25 @@ import classes from './style.module.scss';
 import classNames from "classnames";
 import React from "react";
 import {C_Slider} from "../c_Slider";
+import {C_SliderElementAbout} from "../c_SliderElementAbout";
 
 export const C_SliderVideoAbout = ({className, data}) => {
     const cls = classNames(classes.root, {[className]: className});
-    console.log('123', data.about_company.variableContent)
+
     const {slider, video} = data.about_company.variableContent;
+    console.log(slider.gallery)
+
+    const elements = slider.gallery.map((el, i) => {
+        return <C_SliderElementAbout img={el.src}/>
+
+    })
 
 
     return (
         <div className={cls}>
-            {slider.isVisible && !video.isVisible ? <C_Slider className={classes.С_Slider} isBtnClose={false} items={elements} initialSlide={0} /> : <div>video</div>
-
+            {slider.isVisible && !video.isVisible
+                ? <C_Slider className={classes.С_Slider} isBtnClose={false} items={elements} initialSlide={0} />
+                : <div>video</div>
             }
         </div>
     )
