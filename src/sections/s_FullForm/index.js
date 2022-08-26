e0719f5cbb93b24994ed5e799bb2a552488584a5
tvoygluk <tvoygluk@gmail.com>
2022-08-26 16:50:41 +0300

    Вёрстка формы

--- src/sections/s_FullForm/index.js
+++ src/sections/s_FullForm/index.js
@@ -1,3 +1,4 @@
+import InputMask from 'react-input-mask';
 import classNames from 'classnames';
 import classes from './style.module.scss';
 import {useState} from "react";
@@ -8,6 +9,10 @@ export const S_FullForm = ({className}) => {
     const [phone, setPhone] = useState('');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+
+    const [isPhoneValid, setIsPhoneValid] = useState(true);
+    const [isNameValid, setIsNameValid] = useState(true);
+    const [isEmailValid, setIEmailValid] = useState(true);
 
     return (
         <form className={cls}>
@@ -32,6 +37,71 @@ export const S_FullForm = ({className}) => {
                     }}
                 />
             </div>
+            <InputMask mask="+7-999-999-99-99" value={phone}
+                       onChange={(e) => {
+                           setPhone(prev => e.target.value);
+
+                           if (!e.target.value.length || e.target.value.length > 15 && e.target.value.slice(-1) === '_') {
+                               setIsPhoneValid(false)
+                           } else {
+                               setIsPhoneValid(true)
+                           }
+                       }}>
+                {(inputProps) => (
+                    <input
+                        {...inputProps}
+                        required={true}
+                        className={classNames(classes.inputCommon, classes.inputPhone, {[classes.inputError]: !isPhoneValid})}
+                        type="text"
+                        placeholder={'Введите Ваш номер телефона *'}
+                    />
+                )}
+            </InputMask>
+            <input
+                className={classNames(classes.inputCommon, classes.inputMail, {[classes.inputError]: !isEmailValid})}
+                type="text"
+                placeholder={'Введите Ваш e-mail'}
+                value={email}
+                onChange={(e) => {
+                    setEmail(prev => e.target.value);
+
+                    const validString = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                    const isValid = new RegExp(validString);
+
+                    if (e.target.value.length === 0) {
+                        setIEmailValid(true)
+                    } else if (!isValid.test(e.target.value)) {
+                        setIEmailValid(false)
+                    } else {
+                        setIEmailValid(true)
+                    }
+                }}
+            />
+
+            <div
+                className={classNames(classes.radio, {[classes.radioError]: isRadioError})}
+                onClick={() => setIsRadioError(false)}
+            >
+                <input
+                    className={'visually-hidden'}
+                    id={"private_broker"}
+                    type="radio"
+                    value={"private_broker"}
+                    checked={brokerType === "private_broker"}
+                    onChange={(event) => {
+                        setBrokerType(event.target.value);
+                        setIsRadioError(false);
+                        setCompany('Частный брокер');
+                    }}
+                />
+                <label htmlFor="private_broker">Частный брокер</label>
+            </div>
+
+            <Button
+                onClick={(e) => onButtonClick(e)}
+                buttonWrapperClassName={classes.buttonWrapperClassName}
+                disabled={isProcessing}
+            />
         </form>
     )
 }
