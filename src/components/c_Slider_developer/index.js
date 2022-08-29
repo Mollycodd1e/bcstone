af58f57d71e934f4cb80e906f2fa116736cb24f1
tvoygluk <tvoygluk@gmail.com>
2022-08-29 10:24:53 +0300

    Доработка

---
+++ src/components/c_Slider_developer/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import classes from './style.module.scss';
+import classNames from "classnames";
+
+import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+import {Swiper, SwiperSlide} from "swiper/swiper-react.cjs.js";
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, A11y, Autoplay]);
+
+export const C_Slider = ({className, isBtnClose, items, initialSlide, onBtnCloseClick, setIsPopUpVisible, slidersSpaceBetween = -225, slidesPerView = 3}) => {
+    const cls = classNames(classes.root, { [className]: className });
+    // autoplay={{
+    //     delay: 10000,
+    //         disableOnInteraction: false,
+    // }}
+    return (
+        <div className={cls}>
+            <Swiper
+                slidesPerView={slidesPerView}
+                spaceBetween={slidersSpaceBetween}
+                centeredSlides={true}
+                loop
+                pagination={{
+                    el:'.swiper-pagination',
+                    "clickable": true,
+                    renderBullet: (index, className) => {
+                        return '<span class="' + className + '">' + (index + 1) + "</span>";
+                    },
+                }}
+                className={classNames(classes.swiper, {[classes.swiperMode]: isBtnClose})}
+                initialSlide={Number(initialSlide) || 0}
+            >
+                {items.map((item, i) => {
+                        return (
+                            <SwiperSlide className = {classes.swiperSlide} key={i}>
+                                {item}
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
+            <div className={`swiper-pagination + ${classes.swiper_pagination_custom}`}>
+        </div>
+    )
+}
