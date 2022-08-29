f0f561886478ce32bd4253536291a5a4aada2d83
tvoygluk <tvoygluk@gmail.com>
2022-08-29 18:05:12 +0300

    Доработка - 5

--- src/components/c_Slider_Developer/index.js
+++ src/components/c_Slider_Developer/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import classes from './style.module.scss';
 import classNames from "classnames";
@@ -10,6 +10,29 @@ SwiperCore.use([Navigation, Pagination, A11y, Autoplay]);
 
 export const C_SliderDeveloper = ({className, isBtnClose, items, initialSlide, onBtnCloseClick, setIsPopUpVisible, slidersSpaceBetween = -225, slidesPerView = 3}) => {
     const cls = classNames(classes.root, { [className]: className });
+    const [selectedSlide, setSelectedSlide] = useState(1);
+
+    useEffect(() => {
+        setSelectedSlide(1)
+    },[])
+
+    const [swiperInstance, setSwiperInstance] = useState(null);
+    // const slideTo = (index) => {
+    //     swiperInstance.slideTo(index - 1, 0);
+    // };
+
+    const numSlider = (selected, nums) => {
+        // const nums = [1,2,3,4,5];
+        return (
+            <div className={classes.numbersWrapper} >
+                {nums.map((el, i) => {
+                    const selected = selected === i + 1 ? 'regular__selected' : 'regular';
+                    return <div key={i} className={classes[`${selected}`]} >{i + 1}</div>
+                })}
+            </div>
+        )
+    }
+
     // autoplay={{
     //     delay: 10000,
     //         disableOnInteraction: false,
@@ -24,6 +47,24 @@ export const C_SliderDeveloper = ({className, isBtnClose, items, initialSlide, o
                 pagination={{"clickable": true}}
                 className={classNames(classes.swiper, {[classes.swiperMode]: isBtnClose})}
                 initialSlide={Number(initialSlide) || 0}
+                onSlideChange={
+                    (data) => {
+                        if (data.activeIndex === items.length + 1) {
+                            setSelectedSlide(1);
+                        }
+                        else if (data.activeIndex === 0) {
+                            setSelectedSlide(items.length);
+                        } else {
+                            setSelectedSlide(data.activeIndex - 1);
+                        }
+                        console.log("")
+                        console.log("=============================")
+                        console.log("data.activeIndex     = ", data.activeIndex)
+                        console.log("selectedSlide        = ", selectedSlide)
+                        console.log("=============================")
+                        console.log("")
+                    }
+                }
             >
                 {items.map((item, i) => {
                     return (
@@ -34,6 +75,7 @@ export const C_SliderDeveloper = ({className, isBtnClose, items, initialSlide, o
                 })
                 }
             </Swiper>
+            {numSlider(selectedSlide, items)}
         </div>
     )
 }
