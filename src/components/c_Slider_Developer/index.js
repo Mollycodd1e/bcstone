cbe013b6d5cc8c6712d5aca87f6c09229e6d6350
tvoygluk <tvoygluk@gmail.com>
2022-08-30 10:27:48 +0300

    Доработка - 6

--- src/components/c_Slider_Developer/index.js
+++ src/components/c_Slider_Developer/index.js
@@ -10,24 +10,14 @@ SwiperCore.use([Navigation, Pagination, A11y, Autoplay]);
 
 export const C_SliderDeveloper = ({className, isBtnClose, items, initialSlide, onBtnCloseClick, setIsPopUpVisible, slidersSpaceBetween = -225, slidesPerView = 3}) => {
     const cls = classNames(classes.root, { [className]: className });
-    const [selectedSlide, setSelectedSlide] = useState(1);
-
-    useEffect(() => {
-        setSelectedSlide(1)
-    },[])
-
-    const [swiperInstance, setSwiperInstance] = useState(null);
-    // const slideTo = (index) => {
-    //     swiperInstance.slideTo(index - 1, 0);
-    // };
+    const [selectedSlide, setSelectedSlide] = useState(0);
 
     const numSlider = (selected, nums) => {
-        // const nums = [1,2,3,4,5];
         return (
             <div className={classes.numbersWrapper} >
                 {nums.map((el, i) => {
-                    const selected = selected === i + 1 ? 'regular__selected' : 'regular';
-                    return <div key={i} className={classes[`${selected}`]} >{i + 1}</div>
+                    const paginationClass = selected === i ? 'regular__selected' : 'regular';
+                    return <div key={i} className={classes[`${paginationClass}`]} >{i + 1}</div>
                 })}
             </div>
         )
@@ -49,20 +39,7 @@ export const C_SliderDeveloper = ({className, isBtnClose, items, initialSlide, o
                 initialSlide={Number(initialSlide) || 0}
                 onSlideChange={
                     (data) => {
-                        if (data.activeIndex === items.length + 1) {
-                            setSelectedSlide(1);
-                        }
-                        else if (data.activeIndex === 0) {
-                            setSelectedSlide(items.length);
-                        } else {
-                            setSelectedSlide(data.activeIndex - 1);
-                        }
-                        console.log("")
-                        console.log("=============================")
-                        console.log("data.activeIndex     = ", data.activeIndex)
-                        console.log("selectedSlide        = ", selectedSlide)
-                        console.log("=============================")
-                        console.log("")
+                        setSelectedSlide(data.realIndex);
                     }
                 }
             >
