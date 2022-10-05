b0639bef982c1cddd2f9b3b90dae8ef3d9abdc82
Mollycodd1e <selik7@mail.ru>
2022-10-05 17:54:20 +0300

    Переиспользование слайдера

---
+++ src/components/c_SavingCard/index.js
@@ -0,0 +1,18 @@
+import classes from './style.module.scss';
+import classNames from 'classnames';
+import Image from 'next/image';
+
+export const C_SavingCard = ({className, url, description}) => {
+    
+    const cls = classNames(classes.root, {[className]: className});
+
+    return (
+        <div className={cls}>
+            <div className={classes.card_wrapper}>      
+                <Image src={url} layout='fill'/>
+                <button></button>
+            </div>
+            <p>{description}</p>
+        </div>
+    )
+}
--- src/sections/s_Saving/index.js
+++ src/sections/s_Saving/index.js
@@ -1,15 +1,26 @@
 import classes from './style.module.scss';
 import classNames from 'classnames';
-import { C_SliderSave } from '../../components/c_SliderSave';
+
+import { C_Slider } from '../../components/c_Slider';
+import { C_SavingCard } from '../../components/c_SavingCard';
 
 export const S_Saving = ({className, items}) => {
     
     const cls = classNames(classes.root, {[className]: className});
 
+    const allCards = [];
+
+    items.forEach(item => {
+      allCards.push(<C_SavingCard url={item} description={'Привет'}/>)
+    });
+
+    console.log(allCards)
+    console.log(items)
     return (
         <div className={cls}>          
             <h2>Сохраните сбережения в недвижимости</h2>
-            <C_SliderSave items={items}/>
+            <C_Slider className={classes.saving} items={allCards} initialSlide={0} slidersSpaceBetween = {225} slidesPerView = {1}/>
+            {/* <C_SliderSave items={items}/> */}
         </div>
     )
 }
