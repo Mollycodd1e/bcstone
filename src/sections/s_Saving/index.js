aa1112f4b0fea5af0f434103611dc42d368a41f1
Mollycodd1e <selik7@mail.ru>
2022-10-06 07:25:05 +0300

    ред

--- src/sections/s_Saving/index.js
+++ src/sections/s_Saving/index.js
@@ -19,7 +19,9 @@ export const S_Saving = ({className, items}) => {
     return (
         <div className={cls}>          
             <h2>Сохраните сбережения в недвижимости</h2>
-            <C_Slider className={classes.saving} items={allCards} initialSlide={0} slidersSpaceBetween = {-103} slidesPerView = {1} saving={true}/>
+            <div className={classes.slider_wrapper}>
+                <C_Slider className={classes.saving} items={allCards} initialSlide={0}  slidersSpaceBetween = {-103} slidesPerView = {1} saving={true}/>
+            </div>
         </div>
     )
 }
