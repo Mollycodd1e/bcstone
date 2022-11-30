d80bfb78e561cf27f336f66f35ca528edf4c603e
Mollycodd1e <selik7@mail.ru>
2022-11-30 18:40:03 +0300

    кроп текста

--- src/components/c_PressCard/index.js
+++ src/components/c_PressCard/index.js
@@ -6,7 +6,7 @@ import { useEffect, useRef, useState } from 'react';
 export const C_PressCard = ({className, date, image, title, description}) => {
     
     const cls = classNames(classes.root, {[className]: className});
-
+    const textRef = useRef();
     const [isHover, setHover] = useState(false);
 
     const onHover = function() {
@@ -17,11 +17,15 @@ export const C_PressCard = ({className, date, image, title, description}) => {
         setHover(false)
     }
 
+    const newText = description.slice(0, 100);
+    const spaceNumber = newText.lastIndexOf(' ');
+    const cropText = newText.slice(0, spaceNumber) + '...';
+
     const [isVisible, setVisible] = useState(false);
 
     const dayOfNews = new Date(date).getDate();
     const monthOfNews = new Date(date).getMonth() + 1;
-
+    
     return (
         <div className={classNames(cls, {[classes.element_show]: isVisible})}>
             <div className={classNames(classes.data, {[classes.data_hover]: isHover})}>{dayOfNews}/{monthOfNews}</div>
@@ -32,7 +36,7 @@ export const C_PressCard = ({className, date, image, title, description}) => {
             </div>  
             <div className={classes.description_wrapper}>
               <h3>{title}</h3>
-              <p dangerouslySetInnerHTML={{ __html: description}}></p>
+              <p ref={textRef} dangerouslySetInnerHTML={{ __html: cropText}}></p>
             </div>
         </div>
     )
