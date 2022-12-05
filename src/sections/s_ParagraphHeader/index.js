4722c1ff9df7ab740cdfcda9b1e2b3b213740177
Максим Медяник <tvoygluk@gmail.com>
2022-12-05 21:28:52 +0300

    Новые компоненты

--- src/sections/s_ParagraphHeader/index.js
+++ src/sections/s_ParagraphHeader/index.js
@@ -2,13 +2,13 @@ import classNames from 'classnames';
 import { C_MoreInfoBtn } from '../../components/с_MoreInfoBtn';
 import classes from './style.module.scss';
 
-export const S_MoreInfoBtn = ({className, text, url}) => {
+export const S_ParagraphHeader = ({className, text}) => {
 
     const cls = classNames(classes.root, {[className]: className});
 
     return (
         <div className={cls}>
-            <C_MoreInfoBtn text={text} url={url} />
+            {text}
         </div>
     )
 }
