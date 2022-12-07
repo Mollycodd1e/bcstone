9cb9ddcd4dbbd3caae01765ebcfa2a51a0616c06
Максим Медяник <tvoygluk@gmail.com>
2022-12-07 22:29:23 +0300

    Начал делать news

--- pages/news.js
+++ pages/news.js
@@ -4,30 +4,14 @@ import Script from 'next/script'
 import axios from "axios";
 import Head from 'next/head';
 import classes from  './styleNews.module.scss';
-import { S_Tags } from "../src/sections/s_Tags";
-import { S_Header } from "../src/sections/s_Header";
-import { isNoSubstitutionTemplateLiteral } from "typescript";
-import { S_Video } from "../src/sections/s_Video";
-import { S_Text } from "../src/sections/s_Text";
-import { S_Slider } from "../src/sections/s_Slider";
-import { S_Quote } from "../src/sections/s_Quote";
-import { S_Picture } from "../src/sections/s_Picture";
-import { S_MoreInfoBtn } from "../src/sections/s_MoreInfoBtn";
-import { S_Form } from "../src/sections/s_Form";
+
 import { S_Footer } from "../src/sections/s_Footer";
 import { S_MenuC } from "../src/sections/s_MenuC";
 import {Cc_ComponentGenerator} from "../src/complexComponents/cc_ComponentGenerator";
+import {C_FullForm} from "../src/components/c_FullForm";
+import {S_Popup} from "../src/sections/s_Popup";
 
-// const GenerateComponent = (pageData) => {
-//     const componentCreatorObj = {
-//         "hashtags": () => <S_Tags hashtagList={pageData[0].content.list}/>,
-//     }
-//     if (pageData) {
-//         // console.log("pageData.type", pageData[0].type)
-//         return componentCreatorObj[pageData[0].type]
-//     }
-//     return null;
-// }
+
 
 export default function News() {
     const [width, height] = useWindowSize();
@@ -50,6 +34,8 @@ export default function News() {
     }, []);
 
     const [data, setData] = useState([]);
+    const [newsData, setNewsData] = useState([]);
+    const [shownNews, setShownNews] = useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -70,6 +56,21 @@ export default function News() {
 
     }, []);
 
+    useEffect(() => {
+        const fetchNewsData = async () => {
+            try {
+                const res = await axios.get(`https://stonehedge.ru/api/landing-news/bcstone`, {
+                });
+                const { data } = res;
+                setNewsData(data);
+                return { data };
+            } catch (error) {
+                return { error };
+            }
+        };
+        fetchNewsData();
+    }, []);
+
     const mainPageData = data && data.data && data.data.length !== 0 && data.data.filter(el => {
         if (el && el.id) {
             return el.id === 2
@@ -77,78 +78,6 @@ export default function News() {
     });
 
     const mocks = {
-        hashtagList: [
-            {
-                id: 1,
-                name: 'премия'
-            },
-            {
-                id: 2,
-                name: 'вручение'
-            },
-            {
-                id: 3,
-                name: 'отчет'
-            },
-            {
-                id: 4,
-                name: 'премия'
-            },
-            {
-                id: 5,
-                name: 'вручение'
-            },
-            {
-                id: 6,
-                name: 'отчет'
-            },
-            {
-                id: 7,
-                name: 'отчет'
-            },
-            {
-                id: 8,
-                name: 'премия'
-            },
-            {
-                id: 9,
-                name: 'вручение'
-            },
-            {
-                id: 10,
-                name: 'отчет'
-            },
-            {
-                id: 11,
-                name: 'отчет'
-            },
-            {
-                id: 12,
-                name: 'премия'
-            },
-            {
-                id: 13,
-                name: 'вручение'
-            },
-            {
-                id: 14,
-                name: 'отчет'
-            },
-        ],
-        textList: [
-            {
-                id: 1,
-                text: 'Не следует, однако забывать, что начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки системы обучения кадров, соответствует насущным потребностям. Таким образом сложившаяся структура организации играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям. Таким образом Сложившаяся структура организации способствует подготовки и реализации позиций, занимаемых участниками в отношении поставленных задач. Значимость этих проблем настолько очевидна, что реализация намеченных плановых заданий требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач. Таким образом постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.',                
-            },
-            {
-                id: 2,
-                text: 'Не следует, однако забывать, что начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки системы обучения кадров, соответствует насущным потребностям. Таким образом сложившаяся структура организации играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям. Таким образом Сложившаяся структура организации способствует подготовки и реализации позиций, занимаемых участниками в отношении поставленных задач. Значимость этих проблем настолько очевидна, что реализация намеченных плановых заданий требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач. Таким образом постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.',
-            },
-            {
-                id: 3,
-                text: 'Не следует, однако забывать, что начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки системы обучения кадров, соответствует насущным потребностям. Таким образом сложившаяся структура организации играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям. Таким образом Сложившаяся структура организации способствует подготовки и реализации позиций, занимаемых участниками в отношении поставленных задач. Значимость этих проблем настолько очевидна, что реализация намеченных плановых заданий требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач. Таким образом постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.',
-            }
-        ],
         contacts: {
             phone: '+7 (495) 124-45-67',
             mail: 'sales@bc-stone.ru',
@@ -168,6 +97,7 @@ export default function News() {
 
     const NewsPageData = data.length !==0 ? data.data[1].data : '';
 
+    console.log('newsData', newsData)
     return (
             <Context.Provider value={[width, height]}>
                 <Head>
@@ -187,36 +117,36 @@ export default function News() {
                 {/*<noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7GL33F";height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>*/}
 
                 <div className={"page-wrapper"}>                                  
-                    {data.length !== 0 ? (
+                    {data.length !== 0 && newsData.length !== 0 ? (
                             <>
                                 <div className={`common_top_bg + ${classes.common_top_bg_news}`}  ref={topMenuEl} id="top">
                                     <S_MenuC menuOnTop={menuOnTop} data={mainPageData[0]} />
                                 </div>
-                                {/*<S_Tags hashtagList={NewsPageData[0].content.list}/>*/}
-                                <Cc_ComponentGenerator pageData={NewsPageData} />
-                                {/*<S_Header */}
-                                {/*    header={NewsPageData[1].content.title} */}
-                                {/*    text={NewsPageData[1].content.description}*/}
-                                {/*    data={NewsPageData[1].content.date}*/}
-                                {/*/>*/}
-                                {/*<S_Video url={NewsPageData[2].content.url}*/}
-                                {/*    description={NewsPageData[2].content.description}*/}
-                                {/*/>*/}
-                                {/*<S_Text text={NewsPageData[3].content.text}/>*/}
-                                {/*<S_Slider items={NewsPageData[5].content.gallery} */}
-                                {/*    description={NewsPageData[5].content.description} />*/}
-                                {/*<S_Text text={NewsPageData[3].content.text}*/}
-                                {/*/>*/}
-                                {/*<S_Quote photo={NewsPageData[6].content.photo.src} name={NewsPageData[6].content.name} */}
-                                {/*    description={NewsPageData[6].content.description}*/}
-                                {/*    text={NewsPageData[6].content.text}*/}
-                                {/*/>*/}
-                                {/*<S_Picture src={NewsPageData[4].content.image.src} description={NewsPageData[4].content.description}/>*/}
-                                {/*<S_Text text={NewsPageData[3].content.text}*/}
-                                {/*/>*/}
-                                {/*<S_MoreInfoBtn />*/}
-                                {/*<S_Form header={NewsPageData[8].content.title} description={NewsPageData[8].content.description} ready={NewsPageData[8].content.success}/>*/}
-                                <S_Footer phone_number={contacts.phone} mail={contacts.mail} address={contacts.address} 
+                                {/*<S_Popup isPopupClose={isPopupClose} setIsPopupClose={setIsPopupClose}>*/}
+                                {/*    <C_FullForm data={mainPageData[0]} className={classes.fullFormIndexSection} popup={true}/>*/}
+                                {/*</S_Popup>*/}
+                                {/*<Cc_ComponentGenerator pageData={NewsPageData} />*/}
+
+                                <div>
+                                    {newsData.map((el, i) => {
+                                        return (
+                                            <div key={i} onClick={() => setShownNews(i)}>
+                                                <div>
+                                                    {el.date}
+                                                </div>
+                                                <div>
+                                                    {el.title}
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                <div>
+                                    <img src={newsData[shownNews].image} />
+                                    <div dangerouslySetInnerHTML={{ __html: newsData[shownNews].fullTextWithoutImg}}>
+                                    </div>
+                                </div>
+                              <S_Footer phone_number={contacts.phone} mail={contacts.mail} address={contacts.address}
                                     sales_number={contacts.sales} telegram={contacts.telegram} copyright={copyright}/>
                             </>
                         ) : (
