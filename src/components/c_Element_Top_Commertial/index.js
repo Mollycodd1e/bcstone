a470097273b6e0b55be2b66229a5a8424d27abf2
Mollycodd1e <selik7@mail.ru>
2022-12-23 12:12:13 +0300

    Правка новости

--- src/components/c_Element_Top_Commertial/index.js
+++ src/components/c_Element_Top_Commertial/index.js
@@ -26,11 +26,13 @@ export const C_Element_Top_Commertial = ({className, element}) => {
         cropTitle = title;
         spaceNumberContent = newContent.slice(0, 75).lastIndexOf(' ');
         cropContent = newContent.slice(0, spaceNumberContent) + '...';
+    } else if ((newTitle.length <= 35) && (newContent.length < 40)) {
+        cropTitle = title;
+        cropContent = content;
     } else if ((newTitle.length <= 35) && (newContent.length < 75)) {
         cropTitle = title;
-        // cropContent = content;
         spaceNumberContent = newContent.slice(0, 44).lastIndexOf(' ');
-        cropContent = newContent.slice(0, spaceNumberContent) + '...';
+        cropContent = newContent.slice(0, spaceNumberContent) + '...';  
     } else if ((newTitle.length > 35) && (newTitle.length < 50) && (newContent.length >= 50)) {
         spaceNumberTitle = newTitle.slice(0, 40).lastIndexOf(' ');
         cropTitle = newTitle.slice(0, spaceNumberTitle) + '...';
