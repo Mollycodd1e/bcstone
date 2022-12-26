f67384c2f1543e2c798d3baffbca3a7ea6ece24e
Максим Медяник <tvoygluk@gmail.com>
2022-12-26 14:23:33 +0300

    Добавил классы форме, убрал кнопку Позвоните мне

--- src/components/c_FullForm/index.js
+++ src/components/c_FullForm/index.js
@@ -9,7 +9,7 @@ import { useRef } from 'react';
 import { C_MainButton } from '../c_MainButton';
 
 export const C_FullForm = ({className, data, popup}) => {
-    const cls = classNames(classes.root, {[className]: className});
+    const cls = classNames(classes.root, "form", "form--top", {[className]: className});
     const {description, descriptionSuccess, title, titleSuccess} = data.main_form;
 
     const [width, height] = useContext(Context);
