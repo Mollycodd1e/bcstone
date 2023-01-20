7ec1ea761a7df7d315b96ad531cd2485358254d8
Mollycodd1e <selik7@mail.ru>
2023-01-20 19:47:56 +0300

    Правка слйдера Bottom commercial

--- src/sections/s_Bottom_Commercial/index.js
+++ src/sections/s_Bottom_Commercial/index.js
@@ -58,15 +58,21 @@ export const S_Bottom_Commercial = ({className, data}) => {
                 <div className={classes.sliderWrapper}>
                     <C_Slider className={classes.saving} items={allCards} initialSlide={0} slidesPerView={2}
                     slidersSpaceBetween={
-                        // (width >= sizes.widthMobilePreMd && width < sizes.widthTabletSm) ? -225 :
+                        // (width >= sizes.widthMobileLg && width < sizes.widthTabletSm) ? -225 :
                         (width >= sizes.widthTabletSm && width < sizes.widthTabletMd) ? 15 :
                         (width >= sizes.widthTabletMd && width < sizes.widthNotebook) ? -130 :
                         (width >= sizes.widthNotebook && width < sizes.widthDesktopSm) ? 89 :
                         (width >= sizes.widthDesktopSm && width < sizes.widthDesktopMd) ? -288 :
                         (width >= sizes.widthDesktopMd && width < sizes.widthDesktopLg) ? -137 :
                         width >= sizes.widthDesktopLg ? -137 :
-                        -475}
-                    saving={true} centered={true}/>
+                        // -475
+                    -475
+                    }
+                    saving={true}
+                    centeredSlides={true}
+                    centeredSlidesBounds={true}
+                    centered={true}
+                    />
                 </div>
             </div>
         </div>
